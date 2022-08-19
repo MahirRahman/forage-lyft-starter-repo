637da3502c0d7f8c3a6092352becffb7f87603fa
Mahir Rahman <rahmanm7@miamioh.edu>
2022-08-19 19:47:19 -0400

    Added unit testing to the different car models

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -3,7 +3,6 @@ from engine.engine import Engine
 
 class SternmanEngine(Engine):
     def __init__(self, warning_light_on):
-        super.__init__()
         self.warning_light_on = warning_light_on
 
     def needs_service(self):
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -2,8 +2,7 @@ from engine.engine import Engine
 
 
 class WilloughbyEngine(Engine):
-    def __init__(self, last_service_mileage, current_mileage):
-        super.__init__()
+    def __init__(self, current_mileage, last_service_mileage):
         self.last_service_mileage = last_service_mileage
         self.current_mileage = current_mileage
 
